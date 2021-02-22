e156bf931486dce6710a4aea6f5a6eb282f55d8f
Akif Kartal <akokartal@gmail.com>
2021-02-22 20:55:17 +0300

    add files

---
+++ load and display image/main.py
@@ -0,0 +1,18 @@
+import sys
+
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.uic import loadUi
+
+
+class Start(QDialog):
+    def __init__(self):
+        super(Start,self).__init__()
+        loadUi('first.ui',self)
+
+app = QApplication(sys.argv)
+window = Start()
+window.setWindowTitle("First GUI Window")
+window.setGeometry(100,100,400,300)
+window.show()
+sys.exit(app.exec_())
+
