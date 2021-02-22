d5de7cee0b2a4c2cedc7326efb4c26585ab7eeeb
Akif Kartal <akokartal@gmail.com>
2021-02-22 22:45:49 +0300

    add new work

--- load and display image/main.py
+++ load and display image/main.py
@@ -1,18 +1,48 @@
 import sys
 
+import cv2
+from PyQt5 import QtCore
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
 
 
 class Start(QDialog):
     def __init__(self):
-        super(Start,self).__init__()
-        loadUi('first.ui',self)
+        super(Start, self).__init__()
+        loadUi('gui.ui', self)
+        self.image = None
+        self.myButton.clicked.connect(self.loadClicked)
 
-app = QApplication(sys.argv)
-window = Start()
-window.setWindowTitle("First GUI Window")
-window.setGeometry(100,100,400,300)
-window.show()
-sys.exit(app.exec_())
+    @pyqtSlot()
+    def loadClicked(self):
+        self.loadImage('test.jpg')
 
+    def loadImage(self, fname):
+        self.image = cv2.imread(fname, cv2.IMREAD_GRAYSCALE)
+        self.displayImage()
+
+    def displayImage(self):
+        qformat = QImage.Format_Indexed8
+
+        if len(self.image.shape) == 3:
+            if(self.image.shape[2]) == 4:
+                qformat = QImage.Format_RGBA8888
+            else:
+                qformat = QImage.Format_RGB888
+        img = QImage(
+            self.image, self.image.shape[1], self.image.shape[0], self.image.strides[0], qformat)
+
+        img = img.rgbSwapped()
+        self.imgLabel.setPixmap(QPixmap.fromImage(img))
+        self.imgLabel.setAlignment(
+            QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Start()
+    window.setWindowTitle('Show Image Form')
+    window.show()
+    sys.exit(app.exec_())
